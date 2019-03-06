bfcd1963a5f53f6a1b6a02bf5ca5cf7d61c560df
HoiTemmieColeg <gavintantleff@gmail.com>
2019-03-06 18:02:23 -0500

    Initial Commit

---
+++ src/main/java/io/github/hoitemmiecoleg/simplepvp/SimplePvP.java
@@ -0,0 +1,24 @@
+package io.github.hoitemmiecoleg.simplepvp;
+import org.bukkit.World;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Location;
+
+public class SimplePvP extends JavaPlugin {
+    public boolean disable = false;
+
+    new Location(World pvp1, double 7, double 32, double 5);
+
+    @Override
+    public void onEnable() {
+        getLogger().info("SimplePvP has started.");
+
+        while (disable = false) {
+            if (Location(player) < 1.5) {}
+        }
+
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("SimplePvP has been disabled.");
+    }
+}
