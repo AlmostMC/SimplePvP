cfe311c984212fb8b319a7e3678667526a8f49b1
HoiTemmieColeg <gavintantleff@gmail.com>
2019-03-06 20:21:59 -0500

    modified simplepvp class - checks for world change and if world matches pvp world

--- src/main/java/io/github/hoitemmiecoleg/simplepvp/SimplePvP.java
+++ src/main/java/io/github/hoitemmiecoleg/simplepvp/SimplePvP.java
@@ -1,20 +1,32 @@
 package io.github.hoitemmiecoleg.simplepvp;
+import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Location;
 
-public class SimplePvP extends JavaPlugin {
-    public boolean disable = false;
+import java.util.UUID;
 
-    Location playerLocation = new Location(World pvp1, double 7, double 32, double 5)
+public class SimplePvP extends JavaPlugin {
+    private boolean disable = false;
+    public double pvpX;
+    public double pvpY;
+    public double pvpZ;
+    public World pvp = Bukkit.getWorld("pvp1");
+
+    public void PlayerChangedWorldEvent(Player player) {
+        World from = player.getWorld();
+        if (from.getName().equals(pvp.getName())) {
+
+        }
+    }
+
+    Location playerLocation = new Location(pvp, pvpX, pvpY, pvpZ);
 
     @Override
     public void onEnable() {
         getLogger().info("SimplePvP has started.");
-
-        while (disable = false) {
-            if (playerLocation.distance(player) < 1.5) {}
-        }
 
     }
     @Override
